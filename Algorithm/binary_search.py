55469116a913462fc36400001c5f5a15ebe51522
Arthur Hung <asssde2002@gmail.com>
2024-12-04 19:01:46 +0800

    Add bs iterative version

--- Algorithm/binary_search.py
+++ Algorithm/binary_search.py
@@ -9,7 +9,20 @@ def binary_search(array, start, end, target):
         return binary_search(array, start, mid-1, target)
     else:
         return binary_search(array, mid+1, end, target)
+
+
+def binary_search_iterative(array, start, end, target):
+    while start <= end:
+        mid = (start+end) >> 1
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
     
+    return -1
+
 
 if __name__ == "__main__":
     array = [-14, -3, 0, 5, 10]
@@ -19,4 +32,12 @@ if __name__ == "__main__":
 
     target = -1
     index = binary_search(array, 0, len(array)-1, target)
+    print(index)
+
+    target = 5
+    index = binary_search_iterative(array, 0, len(array)-1, target)
+    print(index)
+
+    target = -1
+    index = binary_search_iterative(array, 0, len(array)-1, target)
     print(index)
